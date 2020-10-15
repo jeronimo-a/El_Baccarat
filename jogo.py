6540f0f076daeda3e22d5397e08b60862fc0b6bb
Jerônimo <jeronimoaa@al.insper.edu.br>
2020-10-15 17:11:20 -0300

    Simplificações em jogo.py

--- jogo.py
+++ jogo.py
@@ -8,19 +8,16 @@ Módulo que contém o loop principal de jogo
 
 # --- CONSTRÓI UM BARALHO COMUM DE 52 CARTAS QUE NÃO SERÁ ALTERADO --- --- ---
 
-# carta: (naipe, número)
+# carta: número
+# não há necessidade de especificar o naipe
 BARALHO = list()
 
-# cria as cartas naipe por naipe:
-# (1: ouros, 2: espadas, 3: copas, 4: paus)
-for naipe in range(1,5):
+# cria quatro grupos de cartas:
+for _ in range(4):
 	
 	# loop de numeração das cartas
-	# (1: ás, 2: dois, 11: valete, 12: dama)
-	for numero in range(1, 14):
-
-		# (3, 1): ás de copas
-		carta = (naipe, numero)
+	# 1: ás, 2: dois, 11: J, 12: Q, 13: K
+	for carta in range(1, 14):
 		BARALHO.append(carta)
 
 BARALHO = tuple(BARALHO)
