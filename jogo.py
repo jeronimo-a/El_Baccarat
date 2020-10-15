9cb1341ddc92fe841384f44f9572b79d4d0d7b34
Jerônimo <jeronimoaa@al.insper.edu.br>
2020-10-15 17:48:08 -0300

    Criação do loop principal e bandeira de jogo

--- jogo.py
+++ jogo.py
@@ -7,3 +7,9 @@ Módulo que contém o loop principal de jogo
 '''
 
 from base import *	# importa todas as funções base
+
+# bandeira de jogo, caso False o programa finaliza
+JOGO = True
+
+# loop principal do jogo, quando acaba o jogo finaliza
+while GAME: pass
