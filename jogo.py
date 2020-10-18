49d6d1a2d6dfef2435bb7255f42052ee8b9d5c79
Jerônimo <jeronimoaa@al.insper.edu.br>
2020-10-18 16:49:23 -0300

    Refatoração de jogo.py

--- jogo.py
+++ jogo.py
@@ -10,102 +10,114 @@ from base import *	# importa todas as funções base
 from falas import falas	# importa as falas do jogo
 from random import shuffle as embaralhar	# importa a função de embaralhamento
 
-# bandeira de jogo, caso False o programa finaliza
-JOGO = True
+# bandeira de programa, caso False, o programa finaliza
+PROGRAMA = True
 
-# loop principal do jogo, quando acaba o jogo finaliza
-while JOGO:
+# loop principal do programa, quando acaba, o programa finaliza
+while PROGRAMA:
 
-	# fala de início de jogo
-	print(falas['bandeiras']['inicio jogo'])
+	# bandeira de jogo, caso False, o jogo termina
+	JOGO = True
 
-	# pergunta com quantos baralhos jogar, verificando erros
-	numero_baralhos = solicitar_entrada(
-		falas['numero baralhos'],
-		'int', whitelist=[1,6,8]
-		)
+	# loop principal de jogo, quando acaba, o jogo em questão finaliza
+	while JOGO:
 
-	# criação do baralho imutável (referência somente)
-	BARALHO = baralhos(numero_baralhos)
+		# fala de início de jogo
+		print(falas['bandeiras']['inicio jogo'])
 
-	# pergunta quantas pessoas vão jogar, verificando erros
-	numero_jogadores = solicitar_entrada(falas['numero jogadores'], 'int')
-
-	# nomeação dos jogadores e atribuição de fichas 
-	FICHAS = dict()	# FICHAS[nome_jogador] = fundos_jogador
-
-	# pergunta o nome dos jogadores
-	for n in range(1, numero_jogadores + 1):
-
-		# solicita o nome dos jogadores, verificando erros
-		nome = solicitar_entrada(
-			falas['nomes'], 'str',
-			blacklist=[''],
-			variavel_pergunta=str(n),
-			marcador_variavel=';'
+		# pergunta com quantos baralhos jogar, verificando erros
+		numero_baralhos = solicitar_entrada(
+			falas['numero baralhos'],
+			'int', whitelist=[1,6,8]
 			)
 
-		# define quantidade de fichas do jogador
-		FICHAS[nome] = 1000
+		# criação do baralho imutável (referência somente)
+		BARALHO = baralhos(numero_baralhos)
 
-	# bandeira de rodada, caso False a rodada termina
-	RODADA = True
+		# pergunta quantas pessoas vão jogar, verificando erros
+		numero_jogadores = solicitar_entrada(falas['numero jogadores'], 'int')
 
-	# loop de rodada, quando acaba a rodada finaliza
-	while RODADA:
+		# nomeação dos jogadores e atribuição de fichas 
+		FICHAS = dict()	# FICHAS[nome_jogador] = fundos_jogador
 
-		# fala de início de rodada
-		print(falas['bandeiras']['inicio rodada'])
+		# pergunta o nome dos jogadores
+		for n in range(1, numero_jogadores + 1):
 
-		# --- --- PASSO 1: APOSTAS --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
-
-		# dicionario de apostas
-		APOSTAS = dict()	# APOSTA[nome_jogador] = (quanto_aposta, qual_aposta)
-
-		# loop de apostas por jogador
-		for nome in FICHAS.keys():
-
-			# pergunta qual é a aposta, verificando erros
-			qual_aposta = solicitar_entrada(
-				falas['qual aposta'], 'str',
-				whitelist=['B', 'J', 'E'],
-				variavel_pergunta=nome,
+			# solicita o nome dos jogadores, verificando erros
+			nome = solicitar_entrada(
+				falas['nomes'], 'str',
+				blacklist=[''],
+				variavel_pergunta=str(n),
 				marcador_variavel=';'
 				)
 
-			# pergunta quanto quer apostar, verificando erros
-			quanto_aposta = solicitar_entrada(
-				falas['quanto aposta'], 'int',
-				maximo=FICHAS[nome],
-				minimo=0
-				)
+			# define quantidade de fichas do jogador
+			FICHAS[nome] = 1000
 
-			# gracinha
-			if quanto_aposta == 0: print(nome, falas['tirar sarro']['sem aposta'])
-			
-			# registra a aposta
-			APOSTAS[nome]= (qual_aposta, quanto_aposta)
-			
+		# bandeira de rodada, caso False a rodada termina
+		RODADA = True
 
-		# --- --- PASSO 2: EMBARALHAR --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+		# loop de rodada, quando acaba a rodada finaliza
+		while RODADA:
 
-		# define o bolo de jogo a partir do baralho de referência
-		BOLO = list(BARALHO)
+			# fala de início de rodada
+			print(falas['bandeiras']['inicio rodada'])
 
-		# embaralha o bolo
-		embaralhar(BOLO)
+			# --- --- PASSO 1: APOSTAS --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 
-		# --- --- PASSO 3: DISTRIBUIR --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+			# dicionario de apostas
+			APOSTAS = dict()	# APOSTA[nome_jogador] = (quanto_aposta, qual_aposta)
 
-		MAO_JOGADOR = []
-		MAO_BANCO = []
+			# loop de apostas por jogador
+			for nome in FICHAS.keys():
 
-		MAO_JOGADOR.append(BOLO.pop())
-		MAO_BANCO.append(BOLO.pop())
-		MAO_JOGADOR.append(BOLO.pop())
-		MAO_BANCO.append(BOLO.pop())
+				# pergunta qual é a aposta, verificando erros
+				qual_aposta = solicitar_entrada(
+					falas['qual aposta'], 'str',
+					whitelist=['B', 'J', 'E'],
+					variavel_pergunta=nome,
+					marcador_variavel=';'
+					)
 
-		
+				# pergunta quanto quer apostar, verificando erros
+				quanto_aposta = solicitar_entrada(
+					falas['quanto aposta'], 'int',
+					maximo=FICHAS[nome],
+					minimo=0
+					)
+
+				# gracinha
+				if quanto_aposta == 0: print(nome, falas['tirar sarro']['sem aposta'])
+				
+				# registra a aposta
+				APOSTAS[nome]= (qual_aposta, quanto_aposta)
+				
+
+			# --- --- PASSO 2: EMBARALHAR --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+			# define o bolo de jogo a partir do baralho de referência
+			BOLO = list(BARALHO)
+
+			# embaralha o bolo
+			embaralhar(BOLO)
+
+			# --- --- PASSO 3: DISTRIBUIR --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+			MAO_JOGADOR = []
+			MAO_BANCO = []
+
+			MAO_JOGADOR.append(BOLO.pop())
+			MAO_BANCO.append(BOLO.pop())
+			MAO_JOGADOR.append(BOLO.pop())
+			MAO_BANCO.append(BOLO.pop())
+
+
+
+
+
+
+
+
 
 
 
